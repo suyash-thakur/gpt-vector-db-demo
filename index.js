8d07b44097cff8f304732ab977fc48deb057512f
suyash-thakur <500067151@stu.upes.ac.in>
2024-06-16 10:44:24 +0530

    version update

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 
-const OpenAI = require('openai');
+const openAI = require('openai');
+
 const { PineconeClient } = require('@pinecone-database/pinecone');
 
 const dotenv = require('dotenv');
@@ -8,123 +9,95 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 
-const openai = new OpenAI({
+const openai = new openAI({
   apiKey: process.env.OPENAI_API_KEY,
 });
 
-const pinecone = new PineconeClient();
 
-const pineconeIndexName = process.env.PINE_CONE_INDEX;
-
-const pineConeNameSpace = process.env.PINE_CONE_NAMESPACE;
-
-
-// OpenAI API for chat completion
-const getChatCompletion = async ({ messages }) => {
-  const config = {
-    model: 'gpt-3.5-turbo',
-    messages,
-  };
-  let response;
-  try {
-    response = await openai.chat.completions.create(config);
-    if (response.choices) {
-      return response.choices[0].message.content;
-    }
-  } catch (error) {
-    console.log('error', error);
+const readWikipediaText = async (isClean = false) => {
+  let wikipediaText;
+  if (isClean) {
+    wikipediaText = fs.readFileSync('./clean-wikipedia.txt', 'utf8');
+  } else {
+    wikipediaText = fs.readFileSync('./wikipedia.txt', 'utf8');
   }
-  return '';
+  return wikipediaText;
 };
 
-
-
-// OpenAI API for creating embeddings
-const createEmbedding = async ({ input }) => {
-  const config = {
-    model: "text-embedding-ada-002",
-    input,
-  };
-
-  let response;
-  try {
-    response = await openai.embeddings.create(config);
-    return response.data;
-  } catch (error) {
-    console.log(error);
-  }
-
-  return '';
-};
-
-// Pinecone API for uploading embeddings
-const uploadTranscriptEmbedding = async ({ index, documents }) => {
-  await index.upsert({
-    upsertRequest: {
-      vectors: documents,
-      namespace: pineConeNameSpace,
-    }
+const cleanWikipediaText = async () => {
+  const wikipediaText = await readWikipediaText();
+  const lines = wikipediaText.split('\n');
+  const cleanLines = lines.map((line) => {
+    const cleanLine = line.replace(/[^a-zA-Z ]/g, "").toLocaleLowerCase();
+    return cleanLine;
   });
+  const cleanText = cleanLines.join('\n');
+  await fs.writeFileSync('./clean-wikipedia.txt', cleanText);
 };
 
 
+const createChunk = async () => {
+  const CHUNK_SIZE = 6;
+  const wikipediaText = await readWikipediaText(true);
+  const lines = wikipediaText.split('\n').filter((line) => line.length > 0)
+  const chunks = [];
 
-// Clean Wikipedia text
-const createCleanFile = async () => {
-  const data = await fs.promises.readFile('./wikipedia.txt', 'utf-8');
-  const lines = data.split('\n').filter(line => line !== '');
-  const cleanLines = lines.map(line => line.replace(/[^a-zA-Z ]/g, "").toLowerCase());
-  const cleanText = cleanLines.join('\n');
-  await fs.promises.writeFile('./clean-wikipedia.txt', cleanText);
+  for (let i = 0; i < lines.length; i += CHUNK_SIZE) {
+    const chunk = lines.slice(i, i + CHUNK_SIZE);
+    chunks.push(chunk.join('\n'));
+  }
+
+  return chunks;
+
 }
 
+const createOpenAIEmbeddings = async (chunk) => {
+  const embeddings = await openai.embeddings.create({
+    input: chunk,
+    model: 'text-embedding-ada-002',
+  });
+  return embeddings;
+};
 
-// Parse text
-const parseText = async () => {
-  const parsedLines = [];
-  const data = await fs.promises.readFile('./clean-wikipedia.txt', 'utf-8');
-  const lines = data.split('\n').filter(line => line !== '');
-  for (const line of lines) {
-    parsedLines.push({
-      text: line,
+const createEmbeddingForChunks = async () => {
+  const chunks = await createChunk();
+  const embeddings = [];
+
+  for (const chunk of chunks) {
+    const embedding = await createOpenAIEmbeddings(chunk);
+    embeddings.push({
+      chunk,
+      embedding,
     });
   }
 
-  return parsedLines;
+
+  return embeddings;
+};
+
+const uploadEmbeddings = async (embeddings, index) => {
+  const embeddingsToUpload = [];
+  for (let i = 0; i < embeddings.length; i += 1) {
+    const embedding = embeddings[i];
+    embeddingsToUpload.push({
+      id: `vec_${i}`,
+      values: embedding.embedding.data[0].embedding,
+      metadata: {
+        text: embedding.chunk,
+      },
+    });
+  }
+  console.log(embeddingsToUpload);
+  await index.upsert({
+    upsertRequest: {
+      vectors: embeddingsToUpload,
+      namespace: pineConeNameSpace,
+    },
+  });
 }
 
-// Create embeddings and upload to Pinecone
-const createAndStoreEmbeddings = async ({ lines, index }) => {
-  for (let i = 0; i < lines.length; i++) {
-    const line = lines[i];
-    const embedding = await createEmbedding({ input: line.text });
-    const pineconeDocument = {
-      id: `vec_${i}`,
-      values: embedding[0].embedding,
-      metadata: {
-        text: line.text,
-      }
-    };
-    await uploadTranscriptEmbedding({ index, documents: [pineconeDocument] });
-  }
-};
-
-// Add embeddings to text
-const addEmbeddingToText = async (index) => {
-  const lines = await parseText();
-  await createAndStoreEmbeddings({ lines, index });
-};
-
-// Get question embedding
-const getGetQuestionEmbedding = async (question) => {
-  const data = await createEmbedding({ input: question });
-  const vector = data[0].embedding;
-  return vector;
-};
-
-// Find most similar
-const findMostSimilar = async (index, vector) => {
-  const queryRequest = {
+const findSimilar = async (index, vector) => {
+  const requestQuery = {
     vector,
     topK: 3,
     includeValues: false,
@@ -132,60 +105,55 @@ const findMostSimilar = async (index, vector) => {
     namespace: pineConeNameSpace,
   }
 
-  try {
-    const queryResponse = await index.query({ queryRequest });
-    console.dir({ queryResponse }, { depth: null });
-    return queryResponse;
-  } catch (e) {
-    console.log(e);
-  }
+  const response = await index.query({ queryRequest: requestQuery });
+  return response;
 };
 
-// Get similar content
-const getSimilarContent = async (index, question) => {
-  const vector = await getGetQuestionEmbedding(question);
-  const response = await findMostSimilar(index, vector);
-  let matchResponse = response.matches.filter(match => match.score > 0.85).map(match => match.metadata.text).join('\n');
-  if (matchResponse.length === 0 && response.matches.length > 0) {
-    matchResponse = response.matches[0].metadata.text;
-  }
-  return matchResponse;
-}
+const createOpenAIResponse = async (question, context) => {
+  const chatCompletion = await openai.chat.completions.create({
+    messages: [
+      { role: 'system', content: `Based on the provided context here give the answer to the question \n ${context}` },
+      { role: 'user', content: question },
+    ],
+    model: 'gpt-3.5-turbo',
+  });
+  return chatCompletion.choices;
+};
 
-// Get chat response
-const getChatResponse = async ({
-  question,
-  content
-}) => {
-  const messages = [
-    {
-      role: 'system',
-      content: `You are a chat assistant. The extract below is from Wikipedia. Answer the question accordingly. Don't deviate outside of the context provided ${content}`,
-    },
-    {
-      role: 'user',
-      content: question,
-    },
-  ];
-  const response = await getChatCompletion({ messages });
-  return response;
-}
+
+
+
+
+const pinecone = new PineconeClient();
+
+const pineconeIndexName = process.env.PINE_CONE_INDEX;
+
+const pineConeNameSpace = process.env.PINE_CONE_NAMESPACE;
+
+const pineConeApiKey = process.env.PINE_CONE_API_KEY;
+
+
+
+
 
 const start = async () => {
-
   await pinecone.init({
     environment: process.env.PINE_CONE_ENVIRIONMENT,
     apiKey: process.env.PINE_CONE_API_KEY,
   });
   const index = await pinecone.Index(pineconeIndexName);
-  await addEmbeddingToText(index);
-  const question = `Which regions were involved in gallic war ?`;
-  const content = await getSimilarContent(index, question);
-  const response = await getChatResponse({
-    question,
-    content,
-  });
-  console.log('response -:', response);
+
+  // await cleanWikipediaText();
+  // const embeddings = await createEmbeddingForChunks();
+  // await uploadEmbeddings(embeddings, index);
+  const question = `WHat is virat kohli's wife name`;
+  const questionEmbedding = await createOpenAIEmbeddings(question);
+  // console.log(questionEmbedding.data[0].embedding);
+  const similarVectors = await findSimilar(index, questionEmbedding.data[0].embedding);
+  console.dir({ similarVectors }, { depth: null });
+  const gptResponse = await createOpenAIResponse(question, similarVectors.matches[0].metadata.text);
+  console.log(gptResponse);
+
 };
 
 start();
